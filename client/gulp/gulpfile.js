d315cf863c12157e7ff8328e1ad1b219d0bfe68d
SimonDolph <simon.dong.sea@hotmail.com>
2015-09-02 23:52:40 +0800

    impl gulp based client

--- client/gulp/gulpfile.js
+++ client/gulp/gulpfile.js
@@ -1,13 +1,40 @@
-var gulp = require('gulp'),
+var path = require('path'),
+    net = require('net'),
+    gulp = require('gulp'),
     connect = require('gulp-connect'),
     watch = require('gulp-watch'),
     less = require('gulp-less'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    gutil = require('gulp-util');
 
 gulp.task('webserver', function() {
+    var Freenoker = {
+        port: 65384,
+        excludes: ['.ico', '.js', '.css']
+    };
+    var client = new net.Socket();
+    client.connect(Freenoker.port, function() {
+        gutil.log('Connected to freenoker server at port ' + Freenoker.port);
+    });
+    client.on('data', function(data) {
+        Freenoker.res.end(data);
+    });
     connect.server({
         livereload: true,
-        root: ['.', '.tmp']
+        root: ['.', '.tmp'],
+        middleware: function(connect, options) {
+            return [function(req, res, next) {
+                var ext = path.extname(req.url);
+                if (Freenoker.excludes.indexOf(ext) >= 0) {
+                    return next();
+                } else {
+                    Freenoker.req = req;
+                    Freenoker.res = res;
+
+                    client.write(req.url);
+                }
+            }];
+        }
     });
 });
 
